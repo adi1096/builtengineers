37ebb163f7df7cab2ec3addf88d78f577b4fddf0
Adi <51466326+adi1096@users.noreply.github.com>
2025-01-15 22:27:33 +1100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import './AboutUs.css';
 
-const AboutUs = () => 
-  <h1>
-    About Us
-  </h1>;
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div className="container1">
+        <div className="column">
+          <div className="service-box">
+            <h3>Experience</h3>
+            <p>Our team brings years of experience to every project, ensuring the highest standards of engineering expertise.</p>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="service-box">
+            <h3>Innovation</h3>
+            <p>We embrace cutting-edge technologies and methodologies, constantly seeking to optimize designs and enhance structural integrity.</p>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="service-box">
+            <h3>Collaboration</h3>
+            <p>We value open communication and collaboration with clients and partners, fostering a shared vision for successful projects.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
