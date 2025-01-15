900f8be9669897b407c4872eef3ea7a39e84dd8a
Adi <51466326+adi1096@users.noreply.github.com>
2025-01-15 22:27:57 +1100

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import email from '../email.png';
+import './ContactUs.css';
 
 const ContactUs = () => {
   return (
-    <>
-      <h1>Contact Us</h1>
-      <div>You can contact us at info@builtengineers.com.au</div>
-    </>
+    <div>
+      <h1>Contact Us and Schedule a Consultation</h1>
+
+      <div className="container2">
+
+        <div className="column">
+          <div className="contact-box">
+            <h3>Email</h3>
+            <img src={email} alt="Email Icon" height="50" />
+            <p>Send us an email at info@builtengineers.com.au for inquiries and project details</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
