8cc8677fff9620eac0d1a567e140682b850c18ba
Adi <51466326+adi1096@users.noreply.github.com>
2025-04-01 22:37:34 +1100

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import picture from '../homepage_2.jpeg'; // Make sure the path is correct
 import './HomePage.css';
+import testvideo from '../testvid.mp4';
 
 const HomePage = () => {
   return (
     <div className="home-page">
+      <video autoPlay loop muted className="home-video">
+        <source src={testvideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="text-overlay">
-        <h1 padding-left= "20px">Built Engineers &amp; Consultants: Building Your Future</h1>
+        <h1>Built Engineers &amp; Consultants: Building Your Future</h1>
       </div>
-      <img src={picture} alt="Home Page Visual" />
     </div>
   );
 };
 
 export default HomePage;
+
